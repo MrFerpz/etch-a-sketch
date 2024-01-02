236555de8caeb4060838a7d0fc3f13a03b1b4d30
Fred <leferpz@gmail.com>
2024-01-02 13:08:33 -0500

    Added ability to choose grid size, but with some issues.

--- etch.js
+++ etch.js
@@ -1,23 +1,38 @@
 const body = document.querySelector("body");
 
+let squaresBtn = document.createElement("button");
+body.appendChild(squaresBtn);
+squaresBtn.textContent = "Choose how big you want the grid!";
+squaresBtn.style.cssText = "margin-bottom: 50px;";
+
+squaresBtn.addEventListener("click", pickNoOfSquares);
+
+function pickNoOfSquares () {
+    let noSquares = prompt("How many squares wide?");
+    if (noSquares <= 50) {
+        for (let y = 0; y <= noSquares; y++) {
+            let line = document.createElement("div");
+            line.classList.add("line");
+            container.appendChild(line);
+            for (let i = 0; i <= noSquares; i++) {
+                let box = document.createElement("div");
+                box.classList.add("box");
+                box.addEventListener("mousedown", () => {
+                    if (box.classList.contains("black")) {
+                        box.classList.remove("black");
+                    } else {
+                        box.classList.add("black")};
+                })
+                line.appendChild(box);
+             }
+        }
+    } else {
+        alert("Please enter a number less than 50.");
+    }
+}
+
 let container = document.createElement("div");
 container.classList.add("container");
 body.appendChild(container);
 
-for (let y = 0; y <= 16; y++) {
-    let line = document.createElement("div");
-    line.classList.add("line");
-    container.appendChild(line);
-    for (let i = 0; i <= 16; i++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        box.addEventListener("mousedown", () => {
-            if (box.classList.contains("black")) {
-                box.classList.remove("black");
-            } else {
-                box.classList.add("black")};
-        })
-        line.appendChild(box);
-     }
-}
 
